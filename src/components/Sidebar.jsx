dc5fc134dc8108e1331392110e675408cea33bdc
yash-166 <n200166@rguktn.ac.in>
2025-03-10 15:55:50 +0530

    one question is changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,21 +20,21 @@ const Sidebar = ({onLogout}) => {
       <div className="space-y-4">
         <button
           onClick={() => handleNavigation("/")}
-          className="w-full p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
+          className="w-full cursor-pointer p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
         >
           Registrations
         </button>
 
         <button
           onClick={() => handleNavigation("/levelOne")}
-          className="w-full p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
+          className="w-full cursor-pointer p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
         >
           Level One
         </button>
 
         <button
           onClick={() => handleNavigation("/levelTwo")}
-          className="w-full p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
+          className="w-full cursor-pointer p-3 text-lg text-white bg-[#1E3A8A] hover:bg-[#2563EB] hover:scale-105 rounded-lg transition duration-300 shadow-md flex justify-center"
         >
           Level Two
         </button>
